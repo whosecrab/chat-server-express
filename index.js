703376a3ddcaec35f017d42766090b4d9530b0ca
Max Moskalchuk <whosecrab@gmail.com>
2023-03-02 15:39:09 +0200

    implement authentication

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('express-async-errors');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
 const authRouter = require('./routes/auth');
 
 const app = express();
@@ -15,8 +18,25 @@ const clientPromise = mongoose
     .connect(process.env.MONGODB_URI)
     .then((m) => m.connection.getClient());
 
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ clientPromise }),
+    })
+);
+
+app.use(passport.authenticate('session'));
 
 app.use('/', authRouter);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,98 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
+passport.use(
+    new LocalStrategy(
+        { usernameField: 'email', passwordField: 'password' },
+        async (email, password, done) => {
+            const user = await User.findOne({ email })
+                .select('+password')
+                .exec();
+
+            if (!user) return done(null, false);
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) return done(null, false);
+
+            user.password = undefined;
+            done(null, user);
+        }
+    )
+);
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        },
+        async (_, __, profile, done) => {
+            const { email, name, picture } = profile._json;
+
+            const user = await User.findOne({ email });
+
+            if (user) return done(null, user);
+
+            const newUser = new User({ email, name, picture });
+            await newUser.save();
+
+            done(null, newUser);
+        }
+    )
+);
+
+passport.serializeUser((user, done) => done(null, user.email));
+
+passport.deserializeUser(async (email, done) => {
+    const user = await User.findOne({ email });
+    if (!user) return done(new Error('User not found'));
+    done(null, user);
+});
+
 const router = express.Router();
+
+router.get('/user', (req, res) => {
+    const { user } = req;
+
+    if (!user) return res.status(401).end();
+
+    res.json(user);
+});
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
     let user = await User.findOne({ email });
     if (user) return res.status(409).end();
 
-    const { username, password } = req.body;
+    const { name, password } = req.body;
     const newUser = new User({
-        username,
+        name,
         email,
         password: bcrypt.hashSync(password, 10),
     });
     await newUser.save();
     res.end();
 });
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    res.json(req.user);
+});
+
+router.get(
+    '/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {
+        successRedirect: process.env.CLIENT_URL,
+    })
+);
 
 module.exports = router;
